1c1c5c9ec551df78fa7403b96db6cc2565984a3c
viaductlights <viaductlights@gmail.com>
2026-06-18 01:38:33 +0200

    debugging moveit in gz sim

--- bean3_description/launch/bean3_bringup.launch.py
+++ bean3_description/launch/bean3_bringup.launch.py
@@ -11,37 +11,30 @@ from launch_ros.actions import Node
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
 
+# testing file for use in conjunction with iss_simulation bean3_gz_test.launch.py
+# does not launch gz sim 
+
 def generate_launch_description():
     # define ros2 packages
-    package_name = 'bean3_description'
+    package_name = 'bean3_description' # for urdf files
     robot_package = get_package_share_directory(package_name)
     gz_package = get_package_share_directory('ros_gz_sim')
+    moveit_package = get_package_share_directory('bean2_moveit')
     default_rviz_config_path = PathJoinSubstitution([robot_package, 'rviz', 'rviz_basic_settings.rviz'])
+    ros2_controllers_yaml = os.path.join(get_package_share_directory('bean2_moveit'), 'config', 'ros2_controllers.yaml')
 
-    # define urdf
-    urdf_file_name = 'bean2.urdf'
-    robot_description_file = os.path.join(robot_package, 'urdf', urdf_file_name)
+    # define urdf.xacro
+    urdf_file = os.path.join(moveit_package, 'config', 'bean2.urdf.xacro')
     robot_description_config = xacro.process_file(
-        robot_description_file
+            urdf_file,
+            mappings={'initial_positions_file':
+                      os.path.join(moveit_package, 'config', 'initial_positions.yaml')}
     )
     
     # GZ environment variable for remapping directory paths
     os.environ["GZ_SIM_RESOURCE_PATH"] = os.path.join(os.path.join(get_package_prefix(package_name), "share"))
 
-    # declare launch arguments
-    gui_arg = DeclareLaunchArgument(
-        name='gui',
-        default_value='true',
-        choices=['true', 'false'],
-        description='Flag to enable joint_state_publisher_gui'
-    )
-
-    model_arg = DeclareLaunchArgument(
-        name='model',
-        default_value=robot_description_file,
-        description='Absolute path to robot URDF file'
-    )
-
+    # rviz argument
     rviz_config_arg = DeclareLaunchArgument(
         name='rvizconfig',
         default_value=default_rviz_config_path,
@@ -60,24 +53,6 @@ def generate_launch_description():
         parameters=[robot_description,{'use_sim_time':True}],
     )
 
-    # Joint state publisher GUI node (runs when gui:=true)
-    joint_state_publisher_gui_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
-    )
-
-    # Regular joint state publisher (runs when gui:=false)
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        condition=UnlessCondition(LaunchConfiguration('gui')),
-        arguments=[LaunchConfiguration('model')]
-    )
-
-    # launch gazebo
-    #gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource(os.path.join(gz_package, 'launch', 'gz_sim.launch.py')),launch_arguments={'gz_args': '-r spacecraft.sdf','use_sim_time':'True'}.items(),)
-
     # spawn robot in gazebo
     spawn = Node(
         package='ros_gz_sim',
@@ -88,14 +63,6 @@ def generate_launch_description():
         output='screen',
     )
 
-    # joint state broadcaster
-    joint_state_broadcaster = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['joint_state_broadcaster'],
-        parameters=[{'use_sim_time':True}],
-    )
-
     # gazebo - ros2 bridge
     bridge = Node(
         package='ros_gz_bridge',
@@ -103,33 +70,43 @@ def generate_launch_description():
         arguments=[
             # clock gz -> ros2
             '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
-            # joint states gz -> ros2
-            '/world/empty/model/bean2/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model',
-        ],
-        remappings=[
-            ('/world/empty/model/bean2/joint_state', 'joint_states'),
         ],
         parameters=[{'use_sim_time':True}],
         output='screen'
+    )
+
+    # joint state broadcaster
+    joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+        parameters=[{'use_sim_time':True}],
+    )
+    
+    # robot controller
+    robot_controller = Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['robot_controller',
+                       'robot_controller', '--param-file', ros2_controllers_yaml],
+            parameters=[{'use_sim_time': True}],
     )
 
     #rviz node
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
-        arguments=['-d', LaunchConfiguration('rvizconfig')]
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        parameters=[{'use_sim_time': True}],
     )
 
     ld = LaunchDescription()
-    ld.add_action(gui_arg)
-    ld.add_action(model_arg)
     ld.add_action(rviz_config_arg)
     ld.add_action(robot_state_publisher)
     ld.add_action(spawn)
     ld.add_action(bridge)
     ld.add_action(joint_state_broadcaster)
-    ld.add_action(joint_state_publisher_gui_node)
-    ld.add_action(joint_state_publisher_node)
+    #ld.add_action(robot_controller)
     ld.add_action(rviz_node)
 
     return ld
--- iss_simulation/launch/bean3_gz_test.launch.py
+++ iss_simulation/launch/bean3_gz_test.launch.py
@@ -11,6 +11,7 @@ def generate_launch_description():
     sim_share = get_package_share_directory('iss_simulation')
     bean3_desc_share = get_package_share_directory('bean3_description')
     gz_package = get_package_share_directory('ros_gz_sim')
+    moveit_share = get_package_share_directory('bean2_moveit')
 
     append_sim_models = AppendEnvironmentVariable(
         name='GZ_SIM_RESOURCE_PATH',
@@ -32,12 +33,22 @@ def generate_launch_description():
             os.path.join(bean3_desc_share, 'launch', 'bean3_bringup.launch.py')
         ])
     )
+    
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gz_package, 'launch', 'gz_sim.launch.py')
         ),
         launch_arguments={
-            'gz_args': os.path.join(sim_share, 'worlds', 'testworld.sdf'),
+            'gz_args': '-r ' + os.path.join(sim_share, 'worlds', 'testworld.sdf'),
+            'use_sim_time': 'True'
+        }.items(),
+    )
+    
+    move_group = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(moveit_share, 'launch', 'move_group.launch.py')
+        ),
+        launch_arguments={
             'use_sim_time': 'True'
         }.items(),
     )
@@ -48,4 +59,5 @@ def generate_launch_description():
     ld.add_action(append_bean_share)
     ld.add_action(bean3_description)
     ld.add_action(gazebo)
+    ld.add_action(move_group)
     return ld
