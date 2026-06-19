4cad0179203d87f0d705451e1e7206498c2aedad
viaductlights <viaductlights@gmail.com>
2026-06-19 14:26:26 +0200

    moveit also working in gz sim for nemo

---
+++ iss_simulation/launch/nemo1_gz_test.launch.py
@@ -0,0 +1,65 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, AppendEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import SetParameter
+
+def generate_launch_description():
+
+    sim_share = get_package_share_directory('iss_simulation')
+    nemo1_desc_share = get_package_share_directory('nemo1_description')
+    gz_package = get_package_share_directory('ros_gz_sim')
+    moveit_share = get_package_share_directory('nemo1_moveit')
+
+    append_sim_models = AppendEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=os.path.join(sim_share, 'models') 
+    )
+
+    append_sim_worlds = AppendEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=os.path.join(sim_share, 'worlds')
+    )
+
+    append_nemo_share = AppendEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=nemo1_desc_share
+    )
+
+    nemo1_description = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(nemo1_desc_share, 'launch', 'nemo1_bringup.launch.py')
+        ])
+    )
+    
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gz_package, 'launch', 'gz_sim.launch.py')
+        ),
+        launch_arguments={
+            'gz_args': '-r ' + os.path.join(sim_share, 'worlds', 'testworld.sdf'),
+            'use_sim_time': 'True'
+        }.items(),
+    )
+    
+    move_group = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(moveit_share, 'launch', 'move_group.launch.py')
+        ),
+        launch_arguments={
+            'use_sim_time': 'True'
+        }.items(),
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(SetParameter(name='use_sim_time', value=True))
+    ld.add_action(append_sim_models)
+    ld.add_action(append_sim_worlds)
+    ld.add_action(append_nemo_share)
+    ld.add_action(nemo1_description)
+    ld.add_action(gazebo)
+    ld.add_action(move_group)
+    return ld
---
+++ nemo1_description/launch/nemo1_bringup.launch.py
@@ -0,0 +1,111 @@
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, SetEnvironmentVariable
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+# testing file for use in conjunction with iss_simulation nemo1_gz_test.launch.py
+# does not launch gz sim 
+
+def generate_launch_description():
+    # define ros2 packages
+    package_name = 'nemo1_description' # for urdf files
+    robot_package = get_package_share_directory(package_name)
+    gz_package = get_package_share_directory('ros_gz_sim')
+    moveit_package = get_package_share_directory('nemo1_moveit')
+    default_rviz_config_path = PathJoinSubstitution([robot_package, 'rviz', 'rviz_basic_settings.rviz'])
+    ros2_controllers_yaml = os.path.join(get_package_share_directory('nemo1_moveit'), 'config', 'ros2_controllers.yaml')
+
+    # define urdf.xacro
+    urdf_file = os.path.join(moveit_package, 'config', 'nemo1.urdf.xacro')
+    robot_description_config = xacro.process_file(
+            urdf_file,
+            mappings={'initial_positions_file':
+                      os.path.join(moveit_package, 'config', 'initial_positions.yaml')}
+    )
+    
+    # GZ environment variable for remapping directory paths
+    os.environ["GZ_SIM_RESOURCE_PATH"] = os.path.join(os.path.join(get_package_prefix(package_name), "share"))
+
+    # rviz argument
+    rviz_config_arg = DeclareLaunchArgument(
+        name='rvizconfig',
+        default_value=default_rviz_config_path,
+        description='Absolute path to rviz config file'
+    )
+
+   
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
+    # robot state publisher
+    robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[robot_description,{'use_sim_time':True}],
+    )
+
+    # spawn robot in gazebo
+    spawn = Node(
+        package='ros_gz_sim',
+        executable='create',
+        parameters=[{'name': 'nemo1',
+                     'topic': 'robot_description',
+                     'use_sim_time':True}],
+        output='screen',
+    )
+
+    # gazebo - ros2 bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            # clock gz -> ros2
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+        ],
+        parameters=[{'use_sim_time':True}],
+        output='screen'
+    )
+
+    # joint state broadcaster
+    joint_state_broadcaster = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+        parameters=[{'use_sim_time':True}],
+    )
+    
+    # robot controller
+    robot_controller = Node(
+            package='controller_manager',
+            executable='spawner',
+            arguments=['robot_controller', '--param-file', ros2_controllers_yaml],
+            parameters=[{'use_sim_time': True}],
+    )
+
+    #rviz node
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+        parameters=[{'use_sim_time': True}],
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(rviz_config_arg)
+    ld.add_action(robot_state_publisher)
+    ld.add_action(spawn)
+    ld.add_action(bridge)
+    ld.add_action(joint_state_broadcaster)
+    ld.add_action(robot_controller)
+    ld.add_action(rviz_node)
+
+    return ld
